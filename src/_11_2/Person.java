69af301d159dd595461bcbe998822e4a0fbef393
alexricardo02 <brinckmannalex@gmail.com>
2025-01-02 02:21:43 +0100

    11.2 empezado

---
+++ src/_11_2/Person.java
@@ -0,0 +1,20 @@
+package _11_2;
+
+public class Person {
+
+    public String name;
+    public String address;
+    public String phoneNumber;
+    public String emailAddress;
+
+    public Person(String name, String address, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public Person() {
+
+    }
+}
---
+++ src/_11_2/Student.java
@@ -0,0 +1,5 @@
+package _11_2;
+
+public class Student {
+
+}
