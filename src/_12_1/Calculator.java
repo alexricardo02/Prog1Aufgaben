7039f5cb2b9585d81de69a269c96e2395b904fcd
alexricardo02 <brinckmannalex@gmail.com>
2025-01-08 23:50:00 +0100

    ss

--- src/_12_1/Calculator.java
+++ src/_12_1/Calculator.java
@@ -1,4 +1,23 @@
 package _12_1;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int first = sc.nextInt();
+        System.out.println("Enter operation");
+        String operation = sc.next();
+        System.out.println("Enter second number");
+        int second = sc.nextInt();
+
+
+
+
+
+    }
 }
